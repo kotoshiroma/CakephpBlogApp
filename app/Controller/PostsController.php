101238b276f2195ed9f547830828ae1ee36f39e2
kotoshiroma <k_shiroma@funteam.co.jp>
2018-03-30 17:49:04 +0900

    delete権限の追加

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -11,7 +11,7 @@ class PostsController extends AppController {
 
 	public function beforeFilter() {
 		parent::beforeFilter();
-		$this->Auth->allow('index', 'view');
+		$this->Auth->allow('index', 'view', 'delete');
 	}
 
 	public function index() {
