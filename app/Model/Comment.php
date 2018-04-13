c700e7d518d9b43cb072294a73b2c6e977dcb598
kotoshiroma <k_shiroma@funteam.co.jp>
2018-04-13 19:25:01 +0900

    バーチャルフィールド「created_fmt」を追加

--- app/Model/Comment.php
+++ app/Model/Comment.php
@@ -4,6 +4,11 @@ App::uses('Model', 'AppModel');
 
 class Comment extends AppModel {
 
+    public $virtualFields = array(
+
+        'created_fmt' => 'DATE_FORMAT(Comment.created,"%Y年%c月%e日 %T")'
+    );
+
     public $belongsTo = array(
         'Post' => array(
             'className' => 'Post'
